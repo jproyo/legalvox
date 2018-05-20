b10f39bb42d8e81596182b445244237d43c69e19
Juan Pablo Royo <juanpablo.royo@gmail.com>
2018-05-20 11:41:15 +0200

    Mock

--- js/index.js
+++ js/index.js
@@ -4,13 +4,37 @@
         e.preventDefault();
         $('#emailDiv').show();
         $('#submitButton').hide();
+        $('#submitButton2').focus();
     });
 
     $('#submitButton2').on('click', function(e){
         e.preventDefault();
-        window.location.href = './contractSign.html'
+        window.location.href = './contractStatus.html'
     });
 
     $('#emailDiv').hide();
+
+    $('div[id^="step"]').hide();
+
+    $('#tipoContrato').on('changeSelect', function(e){
+        $('#stepTipo').show();
+    });
+
+    $('#selectHoras').on('changeSelect', function(e){
+        $('#stepHorario').show();
+    });
+
+    $('#selectDias').on('changeSelect', function(e){
+        $('#stepSueldo').show();
+    });
+
+    $('#inputSueldo').on('changeSelect', function(e){
+        $('#stepDuracion').show();
+    });
+
+    $('#selectDuracion').on('changeSelect', function(e){
+        $('#stepPuesto').show();
+    });
+
 
 })(window);
